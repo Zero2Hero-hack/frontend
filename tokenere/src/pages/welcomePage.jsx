aa9ab47907d91e54b917337e57b9af0350b94b14
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-04-14 14:54:05 +0100

    Third

--- tokenere/src/pages/welcomePage.jsx
+++ tokenere/src/pages/welcomePage.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import Group from '../assets/images/Group.png';
+import '../assets/styles/welcome.css';
 
 function index () {
     return (
         <div className="welcome">
-            <h1> TOKENERE </h1>
-            <button>Skip</button>
+        
+            <button className="sk">Skip</button>
             <div className="img">
                 <img scr= {Group} alt="" />
             </div>
             <h1 className="tok"> Welcome to Tokenere</h1>
+            <div>
+                <button className="_Acct">
+                    <h2 className="_acct">Open Free Account</h2>
+                </button>
+                </div>
+                <div>
+                <button className="Acct">
+                    <h2 className="acct">I have an Account</h2>
+                </button>
+            </div>
         </div>
     );
 };
 
 export default index;
+
