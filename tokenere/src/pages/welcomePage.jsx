372070a86be16a5b7db5ba5ed88907a30a15b868
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-04-19 14:25:19 +0100

    ..

--- tokenere/src/pages/welcomePage.jsx
+++ tokenere/src/pages/welcomePage.jsx
@@ -4,24 +4,50 @@ import '../assets/styles/welcome.css';
 
 function index () {
     return (
-        <div className="welcome">
+      <div className="welcome">
         <nav className="homeNav">
-            <div className="logo">LOGO</div>
-            <div className="sign">
+          <div className="logo">LOGO</div>
+          <div className="sign">
             <button className="signIn">Sign in</button>
             <button className="signUp">Sign up</button>
-            </div>
+          </div>
         </nav>
-            <div className="hero">
-                <img src= { Logo }  alt="hero" />
-            <h1 className="tok"> Welcome to Tokenere</h1>
-            </div>
-            {/* <div>
-               <form>
-                
-                </form> 
-            </div> */}
+        <div className="hero">
+          <img src={Logo} alt="hero" />
+          <h1 className="tok"> Welcome to Tokenere</h1>
         </div>
+        <div>
+          <form>
+            <div className="col-2">
+            <div className="email-input">
+              <label for="email">Email</label>
+              <input
+                type="text"
+                id="text"
+                name="emailAddress"
+                placeholder="Enter your email"
+                //   onChange={formOnChange}
+                //   value={emailAddress}
+              ></input>
+            </div>
+              <label for="email">Password</label>
+              <input
+                type="text"
+                id="text"
+                name="password"
+                placeholder="password"
+                //   onChange={formOnChange}
+                //   value={emailAddress}
+              ></input>
+            <div className="edit-btn">
+              <button> Sign In</button>
+            </div>
+            </div>
+          </form>
+          
+
+        </div>
+      </div>
     );
 };
 
